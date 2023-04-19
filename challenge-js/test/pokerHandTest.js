7346f1e5a024e8a08e64122881fd4844044f299f
Lucas Merchant <lbmerchant93@gmail.com>
2023-04-19 11:35:40 -0600

    Write additional test cases

--- challenge-js/test/pokerHandTest.js
+++ challenge-js/test/pokerHandTest.js
@@ -45,3 +45,59 @@ describe('Rank A Flush', function() {
 });
 
 // More tests go here
+
+describe('Rank A Straight Flush', function() {
+  var hand = new PokerHand('6h 3h 2h 5h 4h');
+
+  it('Return straight flush when hand given', function() {
+    assert.equal(hand.getRank(), 'Straight Flush');
+  });
+});
+
+describe('Rank A Three of a Kind', function() {
+  var hand = new PokerHand('As 8c Ac 10h Ad');
+
+  it('Return three of a kind when hand given', function() {
+    assert.equal(hand.getRank(), 'Three of a Kind');
+  });
+});
+
+describe('Rank A Four of a Kind', function() {
+  var hand = new PokerHand('9s 9c 7h 9h 9d');
+
+  it('Return four of a kind when hand given', function() {
+    assert.equal(hand.getRank(), 'Four of a Kind');
+  });
+});
+
+describe('Rank A Full House', function() {
+  var hand = new PokerHand('2h 4d 2d 4s 2s');
+
+  it('Return full house when hand given', function() {
+    assert.equal(hand.getRank(), 'Full House');
+  });
+});
+
+describe('Rank A High Card', function() {
+  var hand = new PokerHand('10s 6c 3d Ks Qd');
+
+  it('Return high card when hand given', function() {
+    assert.equal(hand.getRank(), 'High Card');
+  });
+});
+
+describe('Rank A Straight', function() {
+  var hand = new PokerHand('6s 8d 10c 9c 7h');
+
+  it('Return straight when hand given', function() {
+    assert.equal(hand.getRank(), 'Straight');
+  });
+});
+
+describe('Rank A Straight With Ace Low', function() {
+  var hand = new PokerHand('4h 2d Ac 3c 5d');
+
+  it('Return straight when hand given with Ace low', function() {
+    assert.equal(hand.getRank(), 'Straight');
+  });
+});
